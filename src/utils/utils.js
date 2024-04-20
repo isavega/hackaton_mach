14a7b6243cbca332ef470d4f3ace68599097ea2d
Maria Francisca Villablanca Adet <mvillablanca@bice.cl>
2024-04-20 12:49:19 -0400

    feat: function rut_dv added

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -8,10 +8,20 @@ export function rutnum(cadena) {
     // Quitar todos los caracteres no numéricos
     return cadena.replace(/\D/g, '');
   }
-  
+
   // Ejemplo de uso:
   //let test_1 = "19688212-k";
   //let test_2 = "19688212-0";
   //let test_func = rutnum(test_1); // usar test 1 o 2
   
   //console.log(test_func);  // Salida: 19688212
+
+// funcion obtener digito verificador
+function rut_dv(cadena) {
+  if (cadena.length === 0) {
+    return null; // Retorna null si la cadena está vacía
+  }
+  
+  return cadena[cadena.length - 1];
+}
+  
