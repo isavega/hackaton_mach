b7d4a0063090a8f7ed87afda945296d8834b2f8e
Maria Francisca Villablanca Adet <mvillablanca@bice.cl>
2024-04-20 11:21:37 -0400

    feat:functions

---
+++ src/utils/utils.js
@@ -0,0 +1,17 @@
+// funcion quitar
+function rutnum(cadena) {
+    // Eliminar el último carácter si es numérico
+    if (/[0-9]$/.test(cadena)) {
+      cadena = cadena.slice(0, -1);
+    }
+  
+    // Quitar todos los caracteres no numéricos
+    return cadena.replace(/\D/g, '');
+  }
+  
+  // Ejemplo de uso:
+  let test_1 = "19688212-k";
+  let test_2 = "19688212-0";
+  let test_func = rutunm(test_1); // usar test 1 o 2
+  
+  console.log(test_func);  // Salida: 19688212
