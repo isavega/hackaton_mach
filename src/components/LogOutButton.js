a5b0b26290159170dc4ba33923066e713c1a9837
isavega <igvega02@gmail.com>
2024-04-20 12:43:52 -0400

    feat: add logout button

---
+++ src/components/LogOutButton.js
@@ -0,0 +1,22 @@
+import Button from "./Button";
+import { useSelector, useDispatch } from "react-redux";
+import { login, logout } from "../redux/slice/authSlice";
+import { supabase } from "../api/supabase";
+
+const LogoutButton = () => {
+  const dispatch = useDispatch();
+  async function signOutUser() {
+    const { error } = await supabase.auth.signOut();
+
+    dispatch(logout());
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    signOutUser();
+  };
+
+  return <Button onClick={handleSubmit}>Logout</Button>;
+};
+
+export default LogoutButton;
