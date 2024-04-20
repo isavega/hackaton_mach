ff8c54cd8ceb311bbcd20214c1487c41a2ae936f
isavega <igvega02@gmail.com>
2024-04-20 12:44:17 -0400

    feat: add logout button in landing

--- src/App.js
+++ src/App.js
@@ -4,14 +4,30 @@ import "./App.css";
 import SignUp from "./components/SignUp";
 import LogIn from "./components/LogIn";
 import Button from "./components/Button";
+import { useSelector } from "react-redux";
+import LogOutButton from "./components/LogOutButton";
 
 function App() {
   const [showLogIn, setShowLogIn] = useState(false);
+
+  const auth = useSelector((state) => state.auth);
+
+  console.log("AUTH: ", auth);
+
+  console.log("USUARIO AUTENTICADO: ", auth.isAuthenticated);
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logoMach} className="App-logo" alt="logo" />
+        {auth.isAuthenticated && (
+          <>
+            <p>¡Hola, {auth.user.user.user_metadata.firstName}!</p>
+
+            <LogOutButton />
+          </>
+        )}
+
         <p>Bienvenido a Mach!</p>
         <Button onClick={() => setShowLogIn(!showLogIn)}>
           {showLogIn ? "Crear cuenta ❤️" : "Iniciar sesión 😇"}
